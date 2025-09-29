045830a0b0ca4d78195558e453665b101100ffc3
Pranshu <88080048+Pranshu1312@users.noreply.github.com>
2025-09-29 17:07:50 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* other config options can go here */
+  eslint: {
+    // This will allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
